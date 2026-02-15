07f339b2e43170e0efc177b193cf98248ee94c0d
a-r-fayiz <arfayiz02@gmail.com>
2026-02-15 20:19:47 +0530

    Added GA4 tracking

--- app.py
+++ app.py
@@ -3,6 +3,19 @@ import pandas as pd
 import numpy as np
 import json
 import plotly.express as px
+
+st.markdown("""
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-945G10EC81"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-945G10EC81');
+</script>
+""", unsafe_allow_html=True)
+
 # Load the cricket data JSON file with caching
 @st.cache_data
 def load_data(json_file):
