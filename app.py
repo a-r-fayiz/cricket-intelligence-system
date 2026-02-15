6acc1a0f39f1245286d8ba8c61d3f6948bc5109d
a-r-fayiz <arfayiz02@gmail.com>
2026-02-15 20:35:53 +0530

    Fix GA4 integration using components

--- app.py
+++ app.py
@@ -3,18 +3,23 @@ import pandas as pd
 import numpy as np
 import json
 import plotly.express as px
+import streamlit.components.v1 as components
 
-st.markdown("""
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-945G10EC81"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'G-945G10EC81');
-</script>
-""", unsafe_allow_html=True)
+components.html(
+    """
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-945G10EC81"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-945G10EC81');
+    </script>
+    """,
+    height=0,
+)
 
 # Load the cricket data JSON file with caching
 @st.cache_data
